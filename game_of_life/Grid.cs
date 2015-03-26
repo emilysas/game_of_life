266b4fd5c2cefb6f2347fa58b860f25b0adab004
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-26 21:55:04 +0000

    passing test - grid starts with 10 columns

--- game_of_life/Grid.cs
+++ game_of_life/Grid.cs
@@ -5,15 +5,22 @@ namespace game_of_life
 	public class Grid
 	{
 		int rows;
+		int columns;
 
 		public Grid ()
 		{
 			rows = 10;
+			columns = 10;
 		}
 
 		public int Rows ()
 		{
 			return rows;
+		}
+
+		public int Columns ()
+		{
+			return columns;
 		}
 	}
 }
