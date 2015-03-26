64a11ea0003cb382a112305e6c90e39d2b2ae468
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-26 22:49:00 +0000

    changing Grid to Board, tests need re-writing

---
+++ game_of_life/Board.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace game_of_life
+{
+	public class Board
+	{
+		Dictionary<string, object> grid;
+		Cell cell;
+
+		public Board ()
+		{
+			grid = new Dictionary<string, object> ();
+			cell = new Cell(0);
+			AddToGrid("A1", cell);
+			AddToGrid("A2", cell);
+			AddToGrid("A3", cell);
+			AddToGrid("A4", cell);
+			AddToGrid("A5", cell);
+			AddToGrid("B1", cell);
+			AddToGrid("B2", cell);
+			AddToGrid("B3", cell);
+			AddToGrid("B4", cell);
+			AddToGrid("B5", cell);
+		}
+
+		public void AddToGrid(string coordinates, object contents)
+		{
+			grid.Add(coordinates, contents);
+		}
+
+		public int CellCount()
+		{
+			return grid.Count;
+		}
+
+
+	}
+}
+
---
+++ game_of_life/BoardTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace game_of_life
+{
+
+	[TestFixture ()]
+	public class GridTest
+	{
+		Board board;
+
+		[SetUp]
+		public void Init ()
+		{
+			board = new Board();
+		}
+
+		[Test ()]
+		public void BoardStartsWithGrid5x5 ()
+		{
+			Assert.AreEqual (25, board.CellCount);
+		}
+			
+	}
+}
+
