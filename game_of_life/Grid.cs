8bcaeb077e32eb959b28dac56f6a7690e282c3f8
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-26 21:50:32 +0000

    passing test - a grid starts with 10 rows

---
+++ game_of_life/Grid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace game_of_life
+{
+	public class Grid
+	{
+		int rows;
+
+		public Grid ()
+		{
+			rows = 10;
+		}
+
+		public int Rows ()
+		{
+			return rows;
+		}
+	}
+}
+
