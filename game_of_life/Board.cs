e70b9abc013b681702e71473ed0f5cd789d2447b
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-27 07:41:21 +0000

    fixing bug

--- game_of_life/Board.cs
+++ game_of_life/Board.cs
@@ -1,55 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace game_of_life
 {
 	public class Board
 	{
-		Dictionary<string, object> grid;
-		Cell liveCell;
-		Cell deadCell;
+		Dictionary<string, Cell> grid;
+		Cell cell;
 
 		public Board ()
 		{
-			grid = new Dictionary<string, object> ();
-			liveCell = new Cell(0);
-			deadCell = new Cell (1);
-			AddToGrid("A1", liveCell);
-			AddToGrid("A2", liveCell);
-			AddToGrid("A3", deadCell);
-			AddToGrid("A4", liveCell);
-			AddToGrid("A5", deadCell);
-			AddToGrid("B1", liveCell);
-			AddToGrid("B2", deadCell);
-			AddToGrid("B3", deadCell);
-			AddToGrid("B4", deadCell);
-			AddToGrid("B5", liveCell);
-			AddToGrid("C1", liveCell);
-			AddToGrid("C2", liveCell);
-			AddToGrid("C3", deadCell);
-			AddToGrid("C4", liveCell);
-			AddToGrid("C5", deadCell);
-			AddToGrid("D1", liveCell);
-			AddToGrid("D2", deadCell);
-			AddToGrid("D3", deadCell);
-			AddToGrid("D4", deadCell);
-			AddToGrid("D5", liveCell);
-			AddToGrid("E1", liveCell);
-			AddToGrid("E2", deadCell);
-			AddToGrid("E3", deadCell);
-			AddToGrid("E4", deadCell);
-			AddToGrid("E5", liveCell);
+			grid = new Dictionary<string, Cell> ();
+			cell = new Cell (0);
+			CreateGrid (cell);
 		}
 
-		public void AddToGrid(string coordinates, object contents)
+		public void AddToGrid(string coordinates, Cell cell)
 		{
-			grid.Add(coordinates, contents);
+			grid.Add(coordinates, cell);
+		}
+
+		public void CreateGrid(Cell cell)
+		{
+			for (int i = 1; i <= 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					int unicode = (0007 + j);
+					StringBuilder gridRef = new StringBuilder ();
+					gridRef.Append ("\\u")
+						.Append (unicode)
+						.Append(i);
+					string coordinate = gridRef.ToString();
+					AddToGrid(coordinate, cell);
+				}
+			}
 		}
 
 		public int CellCount()
 		{
 			return grid.Count;
 		}
+
+
 
 
 	}
