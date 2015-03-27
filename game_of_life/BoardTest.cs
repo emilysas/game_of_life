be6760a436821ae5de443b0f987169c758674d47
Emily Sas <emily.bronwen.sas@gmail.com>
2015-03-27 12:19:45 +0000

    writing test - grid squares contain cells

--- game_of_life/BoardTest.cs
+++ game_of_life/BoardTest.cs
@@ -8,10 +8,12 @@ namespace game_of_life
 	public class GridTest
 	{
 		Board board;
+		Cell cell;
 
 		[SetUp]
 		public void Init ()
 		{
+			cell = new Cell (0);
 			board = new Board(5, 5);
 		}
 
@@ -19,6 +21,12 @@ namespace game_of_life
 		public void BoardStartsWithGrid5x5 ()
 		{
 			Assert.AreEqual (25, board.CellCount());
+		}
+
+		[Test ()]
+		public void EachSquareInGridContainsACell ()
+		{
+			Assert.AreEqual (cell, board.GridRef(0,0));
 		}
 			
 	}
